83f82aa00c0d5de6ba628aa64775a0bf3e40f7c2
Scott Bedard <scottbedard90@gmail.com>
2017-11-16 13:05:16 -0700

    set up testing tools

--- dist/spyfu-vue-analytics.esm.js
+++ dist/spyfu-vue-analytics.esm.js
@@ -1,1 +1,8 @@
-console.log('spyfu-vue-analytics');
+var __cov_aeETtLePrIWijgUdZHJNmA = (Function('return this'))();
+if (!__cov_aeETtLePrIWijgUdZHJNmA.__coverage__) { __cov_aeETtLePrIWijgUdZHJNmA.__coverage__ = {}; }
+__cov_aeETtLePrIWijgUdZHJNmA = __cov_aeETtLePrIWijgUdZHJNmA.__coverage__;
+if (!(__cov_aeETtLePrIWijgUdZHJNmA['C:\\git\\spyfu-vue-analytics\\src\\index.js'])) {
+   __cov_aeETtLePrIWijgUdZHJNmA['C:\\git\\spyfu-vue-analytics\\src\\index.js'] = {"path":"C:\\git\\spyfu-vue-analytics\\src\\index.js","s":{"1":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":35}}},"branchMap":{}};
+}
+__cov_aeETtLePrIWijgUdZHJNmA = __cov_aeETtLePrIWijgUdZHJNmA['C:\\git\\spyfu-vue-analytics\\src\\index.js'];
+__cov_aeETtLePrIWijgUdZHJNmA.s['1']++;console.log('spyfu-vue-analytics');
---
+++ test/specs/analytics.spec.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('spyfu-vue-analytics', () => {
+    it('does something awesome', () => {
+        expect(true).to.be.true;
+    });
+});
