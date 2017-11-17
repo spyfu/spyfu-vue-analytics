5dd1da33a336d4bb824c44d710b2632692134953
Scott Bedard <scottbedard90@gmail.com>
2017-11-17 14:55:01 -0700

    add function to log page views

--- dist/spyfu-vue-analytics.esm.js
+++ dist/spyfu-vue-analytics.esm.js
@@ -15,6 +15,21 @@ var index = {
         else if (typeof options.events !== 'object' || Array.isArray(options.events)) {
                 warn('Invalid configuration, "events" must be an object.');
             }
+
+            // log page views if a function was passed in
+            else if (typeof options.logPageView !== 'undefined') {
+                    if (typeof options.logPageView === 'function') {
+                        Vue.mixin({
+                            created() {
+                                if (this.$root === this) {
+                                    this.$watch('$route', options.logPageView);
+                                }
+                            }
+                        });
+                    } else {
+                        warn('Invalid configuration, "logPageView" must be a function.');
+                    }
+                }
     }
 };
 
--- test/specs/analytics.spec.js
+++ test/specs/analytics.spec.js
@@ -1,34 +1,30 @@
-const { expect } = require('chai');
-const { factory } = require('spyfu-vue-factory');
-const sinon = require('sinon');
 const Plugin = require('../../');
-const Vue = require('vue');
-
-//
-// factory
-//
-const mount = factory();
+const VueRouter = require('vue-router');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { createLocalVue } = require('vue-test-utils');
 
 //
 // tests
 //
 describe('spyfu-vue-analytics', () => {
 
+    let Vue;
+
+    // create a local copy of the Vue constructor so we
+    // can install the plugin multiple times in isolation
+    beforeEach(() => {
+        Vue = createLocalVue();
+    });
+
     //
     // installation
     //
     describe('installation', () => {
-
-        // uninstall our plugin before each test
-        beforeEach(() => {
-            delete Vue.prototype.$logEvent;
-        });
-
         it('warns if installed without config object', () => {
             const warn = sinon.stub(console, 'warn');
-            const VueConstructor = Object.create(Vue);
 
-            VueConstructor.use(Plugin);
+            Vue.use(Plugin);
 
             expect(warn.called).to.be.true;
             expect(warn.lastCall.args[0]).to.include('Missing plugin configuration');
@@ -38,9 +34,8 @@ describe('spyfu-vue-analytics', () => {
 
         it('warns if installed without an "events" object', () => {
             const warn = sinon.stub(console, 'warn');
-            const VueConstructor = Object.create(Vue);
 
-            VueConstructor.use(Plugin, {});
+            Vue.use(Plugin, {});
 
             expect(warn.called).to.be.true;
             expect(warn.lastCall.args[0]).to.include(
@@ -48,6 +43,42 @@ describe('spyfu-vue-analytics', () => {
             );
 
             warn.restore();
+        });
+
+        it('warns if installed with an invalid "logPageView" function', () => {
+            const warn = sinon.stub(console, 'warn');
+
+            Vue.use(Plugin, { events: {}, logPageView: 'foo' });
+
+            expect(warn.called).to.be.true;
+            expect(warn.lastCall.args[0]).to.include(
+                'Invalid configuration, "logPageView" must be a function'
+            );
+
+            warn.restore();
+        })
+    });
+
+    //
+    // usage
+    //
+    describe('usage', () => {
+        it('logs page views', (done) => {
+            const logPageView = sinon.stub();
+
+            Vue.use(VueRouter);
+            Vue.use(Plugin, { events: {}, logPageView })
+
+            const vm = new Vue({
+                router: new VueRouter(),
+            });
+
+            vm.$router.push({ query: { foo: 'bar' }});
+
+            vm.$nextTick(() => {
+                expect(logPageView.called).to.be.true;
+                done();
+            });
         });
     });
 });
