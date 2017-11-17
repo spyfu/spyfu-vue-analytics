e19f310c5134466f796329d76a36b5ea55cb854c
Scott Bedard <scottbedard90@gmail.com>
2017-11-17 16:32:42 -0700

    dist

--- dist/spyfu-vue-analytics.js
+++ dist/spyfu-vue-analytics.js
@@ -9,8 +9,18 @@ var warn = function (msg) {
     console.warn('[SpyfuVueAnalytics] ' + msg);
 };
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+  return typeof obj;
+} : function (obj) {
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+};
+
 var index = {
-    install(Vue, options = null) {
+    install: function install(Vue) {
+        var _this = this;
+
+        var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
 
         // options must be provided
         if (options === null) {
@@ -18,7 +28,7 @@ var index = {
         }
 
         // make sure an events object was provided
-        else if (typeof options.events !== 'object' || Array.isArray(options.events)) {
+        else if (_typeof(options.events) !== 'object' || Array.isArray(options.events)) {
                 warn('Invalid configuration, "events" must be an object.');
             }
 
@@ -28,7 +38,7 @@ var index = {
                         warn('Invalid configuration, "logPageView" must be a function.');
                     } else {
                         Vue.mixin({
-                            created() {
+                            created: function created() {
                                 if (this.$root === this) {
                                     this.$watch('$route', options.logPageView);
                                 }
@@ -38,28 +48,32 @@ var index = {
                 }
 
         // and finally, attach our logEvent method to the Vue prototype
-        Vue.prototype.$logEvent = (name, payload) => this.logEvent(options, name, payload);
+        Vue.prototype.$logEvent = function (name, payload) {
+            return _this.logEvent(options, name, payload);
+        };
     },
-    logEvent(options, name, payload) {
-        const { events, handlers } = options;
+    logEvent: function logEvent(options, name, payload) {
+        var events = options.events,
+            handlers = options.handlers;
 
         // make sure the given event exists
+
         if (typeof events[name] === 'undefined') {
-            warn(`Unknown event "${name}".`);
+            warn('Unknown event "' + name + '".');
         }
 
         // make sure the given event value is an object
-        else if (typeof events[name] !== 'object') {
-                warn(`Invalid configuration for "${name}" event, value must be an object.`);
+        else if (_typeof(events[name]) !== 'object') {
+                warn('Invalid configuration for "' + name + '" event, value must be an object.');
             }
 
             // finally, call the given event handlers
             else {
-                    Object.keys(events[name]).forEach(key => {
+                    Object.keys(events[name]).forEach(function (key) {
 
                         // make sure the given handler is defined
                         if (typeof handlers[key] !== 'function') {
-                            warn(`Missing "${key}" handler for event "${name}".`);
+                            warn('Missing "' + key + '" handler for event "' + name + '".');
                         }
 
                         // otherwise call the given handler
